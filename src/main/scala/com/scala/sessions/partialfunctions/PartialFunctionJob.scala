e4633f51e43410fa1b96962f004daf009e865e87
Sujesh Chirackkal <sujesh.chirackkal@gmail.com>
2015-11-04 03:54:28 +1100

    adds answer for PartialFunction exercise

---
+++ src/main/scala/com/scala/sessions/partialfunctions/PartialFunctionJob.scala
@@ -0,0 +1,28 @@
+package com.scala.sessions.partialfunctions
+
+object PartialFunctionJob {
+
+  // partial function to compute 30 / x, which is not defined for x = 0
+  def fraction = new PartialFunction[Int, Int] {
+    def apply(i: Int) = 30 / i
+    def isDefinedAt(i: Int) = i != 0
+  }
+
+  // partial function to increment the integer elements by 1 in a list
+  def incAny: PartialFunction[Any, Int] = { case i: Int => i + 1}
+
+  def main (args: Array[String]): Unit = {
+    val y = List(10, 20, 30, 0) collect fraction
+    println("y = " + y)
+
+    val x = List(1, "AAAA", 2) collect incAny
+    println(" x = " + x)
+
+    println(" checking if fraction is defined for 0 " + fraction.isDefinedAt(0))
+
+    // following call should thorw an ArithmeticException
+    val z = fraction(0)
+    println ("z = " + z)
+
+  }
+}
