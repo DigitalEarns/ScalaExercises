9757c75ffb2d2ba202402138e993f337dbb3762c
Sujesh Chirackkal <sujesh.chirackkal@gmail.com>
2015-11-13 03:48:05 +1100

    answer to etl job, with exception handling using Either, Left and Right

--- src/main/scala/com/scala/sessions/etl/EtlFixedLengthRecordWithExceptionHandling.scala
+++ src/main/scala/com/scala/sessions/etl/EtlFixedLengthRecordWithExceptionHandling.scala
@@ -7,28 +7,31 @@ import scala.util.Try
  * for handling exceptions
  */
 
-
 case class MetaData2[T](name: String, startIndex: Int, endIndex: Int, convert: String => Either[String, T])
 
-sealed trait TypeCheckEither[String, T] {
+sealed trait TypeCheckEither2[String, T] {
   def toType(x: String): Either[String, T]
 }
 
-object IntType2 extends TypeCheckEither[String, Int] {
+object IntType2 extends TypeCheckEither2[String, Int] {
+  // function returns an Instance of String (for failure, it is a message) or Int (if value is converted successfully)
   def toType (x: String): Either[String, Int] = {
-    Try(x.toInt).toOption match {
-      case Some(i) => Right(i)
-      case None    => Left(s"Can not convert $x to Int")
+    if (Try(x.toInt).isFailure) {
+      Left(s"Unable to convert $x to Int")
     }
+    else
+      Right(x.toInt)
   }
 }
 
-object DoubleType2 extends TypeCheckEither[String, Double] {
+object DoubleType2 extends TypeCheckEither2[String, Double] {
+  // function returns an Instance of String (for failure, it is a message) or Int (if value is converted successfully)
   def toType (x: String): Either[String, Double] = {
-    Try(x.toDouble).toOption match {
-      case Some(i) => Right(i)
-      case None    => Left(s"Can not convert $x to Double")
+    if (Try(x.toDouble).isFailure) {
+      Left(s"Can not convert $x to Double")
     }
+    else
+      Right(x.toDouble)
   }
 }
 
@@ -44,24 +47,22 @@ object SampleFileSchema2 {
 
 object EtlFixedLengthRecordWithExceptionHandling {
   // function to decode a record as per the metadata (based on index)
-  def decoder(record: String, schemaDetails: List[MetaData[_]]) = {
+  def decoder(record: String, schemaDetails: List[MetaData2[_]]) = {
     schemaDetails.map {
       f => {
         // if the type is not matched, field value is defaulted to ""
         f.convert(record.substring(f.startIndex, f.endIndex).trim) match {
-          case Some(x) => x
-          case None    => {
-            ""
-          }
+          case Right(x) => x
+          case Left(x)  => x
         }
       }
     }
   }
 
   // converting the decodedRecord of type List to a Map, by making field name as the Key
-  def decodedRecordsToMap (record: List[String], fieldSchema: List[MetaData[_]]) = {
+  def decodedRecordsToMap (record: List[String], fieldSchema: List[MetaData2[_]]) = {
     // get the fieldNames alone from the schema
-    val fieldNames = fieldSchema.map((m: MetaData[_]) => m.name)
+    val fieldNames = fieldSchema.map((m: MetaData2[_]) => m.name)
 
     // Decode the input record one by one to a List of values per record
     val decodedRecordsList = record.map(decoder(_, fieldSchema))
@@ -72,13 +73,13 @@ object EtlFixedLengthRecordWithExceptionHandling {
 object EtlFixedLengthRecordJob2 {
   def main(args: Array[String]): Unit = {
     // input records are specified as List in this code, within this object itself (which wont be the case in real)
-    val input = List(
+    val input2 = List(
       "SUJESHCHIRACKKAL 179870 KOCHI  DigitalEnterprise 2000.00",
       "SNEHAL VIDYAN    232234 SYDNEY BankingDomain     1800.00",
       "VINEETH MM       444455 SYDNEY DataProducts      3000.60",
       "111111111        SSSSSS @!@#@$ $$$$$$$$$$$$$$$$$ !#$@#$@"
     )
-    val recordMap = EtlFixedLengthRecord.decodedRecordsToMap(input, SampleFileSchema.mainSchema)
+    val recordMap = EtlFixedLengthRecordWithExceptionHandling.decodedRecordsToMap(input2, SampleFileSchema2.mainSchema)
     recordMap.foreach(println(_))
   }
 }
