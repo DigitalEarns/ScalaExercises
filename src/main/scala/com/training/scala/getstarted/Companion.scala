f31eac086b8d0e3088027f9a52581446fa6f495a
vineethMM <vineethmanayilparambil.mohanan@cba.com.au>
2016-01-30 15:19:42 +1100

    add comments for Companion object and Option type

--- src/main/scala/com/training/scala/getstarted/Companion.scala
+++ src/main/scala/com/training/scala/getstarted/Companion.scala
@@ -5,27 +5,47 @@ package com.training.scala.getstarted
  */
 
 // Companion Class
-class Companion(argString: String) {
+class MyClass(argString: String) {
   private var extraData = ""
   override def toString = argString + extraData
 }
 
-// object name is same as the class name. (Companion Object)
-object Companion {
-  def apply(base: String, extra: String): Companion = {
-    val s = new Companion(base)
+/*
+ * Companion Object: object name is same as the class name.
+ *
+ * Please note that the Companion object must be defined in same `.scala`
+ * source file as of the Companion Class.
+ *
+ * The common use case for companion object is to define singleton methods
+ * and values. Its roughly equivalent to static methods and members in Java.
+ *
+ */
+object MyClass {
+  /* The `apply` method in Scala Companion objects are used for syntactic sugar
+   * The following invocation will demonstrate this
+   * */
+  def apply(base: String, extra: String): MyClass = {
+    val s = new MyClass(base)
     s.extraData = extra
     s
   }
 
-  // Usage of apply method. It is useful when you have a common operation to be done whenever you create
-  // an object
-  def apply(base: String) = new Companion(base)
+  // overloaded `apply` method
+  def apply(base: Int) = new MyClass(base.toString)
 }
 
 object RunJob {
   def main(args: Array[String]) {
-    println(Companion("Hello", "Scala"))
-    println(Companion("Hello"))
+
+    // Invoking the `apply` methods of Companion object.
+    println(MyClass.apply("Hello", "Scala"))
+    println(MyClass.apply(111))
+
+    // Scala provides a Syntatic sugar, which makes the
+    // following statements perfectly legal and gives the same result
+    // as above
+
+    println(MyClass("Hello", "Scala")) // The `apply` methods are not called explicitly,
+    println(MyClass(111))              // Invocations looks like these factory methods are
   }
 }
--- src/main/scala/com/training/scala/getstarted/OptionObject.scala
+++ src/main/scala/com/training/scala/getstarted/OptionObject.scala
@@ -1,12 +1,37 @@
 package com.training.scala.getstarted
 
 /**
- * An exmaple on the usage of Options
+ * An example on the usage of Options.
+ *
+ * Option is a data type in Scala that explicitly tells the user
+ * about absence of some value. If you have worked with java, you
+ * might have come across with NullPointer exceptions at some time.
+ * It happens when you are expecting some value and you end up with a
+ * null reference which not not properly handled. A common example is
+ * we have lookup Map<String, String>. If we perform a `get` operation
+ * with a key that is not there in the map, it will return a null
+ * (absence of a value), but the return type of the `get` method (`java.lang.String`)
+ * doesn't explicitly tell this.
+ *
+ * The Option data type avoids this scenario by making it explicit that
+ * there is a chance of absence of value of particular Type.
+ *
+ *
  */
-
 object OptionObject {
   def main(args: Array[String]) {
-    // a simple example, Map returns Option[T] for its get method
+
+    // In Scala the Map's get operation returns an Option.
+    // That is if we have a Map[String, String]. The signature of `get` method is
+    //
+    //  def get(arg: String): Option[String].
+    //
+    // The Option[T] has exactly two subclasses  Some[T] and None.
+    // Where `Some[T]` indicates (& wraps the value itself) presence of value of type T
+    // and `None` indicates absence of value of Type T
+    //
+    // thus by saying the return type of the method is Option[String],
+    // its clear that the `get` operation may or may not return a value of type String
     val capitals = Map(
       "France"    -> "Paris",
       "India"     -> "Delhi",
@@ -18,23 +43,26 @@ object OptionObject {
     println(capitals.get("France"))
     println(capitals.get("Argentina"))
 
-    toInt("10") match {
-      case Some(x) => println(s"Successfully converted to $x")
-      case None    => println("Can not be converted")
-    }
-
-    toInt("ABCD") match {
-      case Some(x) => println(s"Successfully converted to $x")
-      case None    => println("Can not be converted")
-    }
+    checkPresenceOfInteger(toInt("10"))
+    checkPresenceOfInteger(toInt("ABC"))
   }
 
   // function to convert a given string to Int
+  // By making the return type as Option[Int] its
+  // clear from the definitions itself that the function
+  // may or may not return a value of type Int.
   def toInt(in: String): Option[Int] = {
     try {
       Some(Integer.parseInt(in.trim))
     } catch {
       case e: NumberFormatException => None
     }
+  }
+
+  def checkPresenceOfInteger(arg: Option[Int]) = arg match {
+    // code to hanlde the integer value
+    case Some(x) => println(s"An integer value is present = $x")
+    // code to handle the absence of integer value
+    case None    => println("Denotes absence of integer value")
   }
 }
